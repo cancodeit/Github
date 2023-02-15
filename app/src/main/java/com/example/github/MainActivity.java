0defaf12ebefbea001471cf4ace55a7681708217
CkChoudhary <cancodit@gmail.com>
2023-02-15 21:18:16 +0530

    First Time Uploading in github

---
+++ app/src/main/java/com/example/github/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //stages
+        //1st Git Addd
+        //2nd Commit
+    }
+}
